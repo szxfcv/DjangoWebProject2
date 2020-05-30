4888c2bcb952f68ae899d4f5aec01c9fedb68bc7
szxfcv <B_Washio_Lutwidge@outlook.jp>
2020-05-30 14:12:21 +0900

    fix typo

--- DjangoWebProject2/HelloDjangoApp/views.py
+++ DjangoWebProject2/HelloDjangoApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def indec(request):
+def index(request):
     return HttpResponse("Hello Django!")
 
 # Create your views here.
