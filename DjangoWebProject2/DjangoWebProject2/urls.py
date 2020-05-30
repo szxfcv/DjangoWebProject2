c5add94c1f04e8c00e7a553601e22cff63fede28
szxfcv <B_Washio_Lutwidge@outlook.jp>
2020-05-30 14:09:55 +0900

    Add HelloDjangoApp

--- DjangoWebProject2/DjangoWebProject2/urls.py
+++ DjangoWebProject2/DjangoWebProject2/urls.py
@@ -20,7 +20,11 @@ Including another URLconf
 #from django.contrib import admin
 #from django.urls import path
 
+from django.conf.urls import include, url
+import HelloDjangoApp.views
 urlpatterns = [
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
     # Uncomment the next line to enable the admin:
     #path('admin/', admin.site.urls)
 ]
---
+++ DjangoWebProject2/HelloDjangoApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def indec(request):
+    return HttpResponse("Hello Django!")
+
+# Create your views here.
