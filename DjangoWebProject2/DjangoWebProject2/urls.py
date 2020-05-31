18cc95a818c9ccada592bb2cc63a0e998d06a326
szxfcv <B_Washio_Lutwidge@outlook.jp>
2020-05-31 12:31:53 +0900

    Chapter 6

--- DjangoWebProject2/DjangoWebProject2/urls.py
+++ DjangoWebProject2/DjangoWebProject2/urls.py
@@ -25,6 +25,7 @@ import HelloDjangoApp.views
 urlpatterns = [
     url(r'^$', HelloDjangoApp.views.index, name='index'),
     url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    url(r'^about$', HelloDjangoApp.views.about, name='about'),
     # Uncomment the next line to enable the admin:
     #path('admin/', admin.site.urls)
 ]
